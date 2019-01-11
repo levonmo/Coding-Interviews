4f09298f4668e254a9a3805f23aec5118be8c6c7
mowenlong <741321320@qq.com>
2019-01-11 19:46:18 +0800

    滑动窗口的最大值

---
+++ 滑动窗口的最大值.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+/**
+ * 给定一个数组和滑动窗口的大小，找出所有滑动窗口里数值的最大值。
+ * 例如，如果输入数组{2,3,4,2,6,2,5,1}及滑动窗口的大小3，那么一共存在6个滑动窗口，他们的最大值分别为{4,4,6,6,6,5}；
+ * 针对数组{2,3,4,2,6,2,5,1}的滑动窗口有以下6个： {[2,3,4],2,6,2,5,1}， {2,[3,4,2],6,2,5,1}， {2,3,[4,2,6],2,5,1}，
+ *                                              {2,3,4,[2,6,2],5,1}， {2,3,4,2,[6,2,5],1}， {2,3,4,2,6,[2,5,1]}。
+ */
+public class 滑动窗口的最大值 {
+
+
+    public static ArrayList<Integer> maxInWindows(int[] num, int size) {
+        ArrayList<Integer> list = new ArrayList<>();
+        if (size == 0) return list;
+        for (int i = size - 1; i < num.length; i++) {
+            int max = max(num, i - (size - 1), i);
+            list.add(max);
+        }
+        return list;
+    }
+
+    private static int max(int[] arr,int start,int end) {
+        int max = arr[start];
+        for (int i = start + 1; i <= end; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+}
